93b00a8e809edeb3abfbf931fb98bd50085b84d5
Josh Urbaniak <joshurb@msn.com>
2019-04-28 14:19:22 -0700

    included redux tested api endpoint

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
+    //dd($request);
+    return 'user: '.$request->user();
 });
