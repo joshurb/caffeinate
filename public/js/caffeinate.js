9d8e21a6b7b4061dfd5b78b0eb6b0bb2431e836d
Josh Urbaniak <joshurb@msn.com>
2019-04-29 22:38:17 -0700

    wired endpoints to application

---
+++ public/js/caffeinate.js
@@ -0,0 +1,94 @@
+
+$('document').ready(function(){
+
+
+
+    var app = new Vue({
+    el: '#app',
+    data: {
+    drinks: null,
+    consumedDrinks: null,
+    suggestedNextDrink: null,
+    toDrink: null,
+    totalCaffeine: 0,
+    caffeineLeft: ''
+        },
+        mounted () {
+            this.caffeineRemaining();
+            this.consumed();
+            this.suggestDrink();
+            this.drinksAvailable();
+
+        },
+        methods: {
+            suggestDrink: function(){
+                axios
+                    .get('http://caffeinate.test/api/suggest-drink')
+                    .then(response => (this.suggestedNextDrink = response.data));
+            },
+            caffeineRemaining: function(){
+                axios
+                    .get('http://caffeinate.test/api/total-caffeine-remaining')
+                    .then(response => (this.caffeineLeft = response.data.totalCaffeineRemaining));
+            },
+            consumed: function(){
+                axios
+                    .get('http://caffeinate.test/api/drinks-consumed')
+                    .then(response => (this.consumedDrinks = response.data));
+            },
+            drinksAvailable: function(){
+                axios
+                .get('http://caffeinate.test/api/drinks')
+                .then(response => (this.drinks = response.data));
+            },
+            checkAndDrink: function(toDrink){
+                if(this.caffeineLeft - toDrink.caffeine_amount * toDrink.servings > 0)
+                    this.drinkADrink(toDrink.id);
+                else{
+                    $('.modal').modal('toggle');
+                    console.log('too much caffeine');
+                }
+
+            },
+            drinkADrink: function(id){
+                let drink = { drink_id: id };
+                axios
+                    .post('http://caffeinate.test/api/drank', drink)
+                    // .then(response => (app.consumedDrinks = response.data));
+                    .then(function(response){
+                        // this.consumedDrinks = response.data;
+                        app.consumed();
+                        app.caffeineRemaining();
+                        app.suggestDrink();
+                    })
+            },
+            deleteDrink: function(id){
+                axios
+                    .delete('http://caffeinate.test/api/delete-consumed-drink/'+id)
+                    // .then(response => (app.consumedDrinks = response.data));
+                    .then(function(response){
+                        app.consumed();
+                        app.caffeineRemaining();
+                        app.suggestDrink();
+                    })
+            }
+        }
+    });
+
+
+
+});
+
+// function drinkADrink(element){
+//     // console.log(element);
+//      let drink = { drink_id: element.dataset.drinkId };
+//     axios
+//         .post('http://caffeinate.test/api/drank', drink)
+//         .then(response => (app.consumedDrinks = response.data));
+// }
+
+// function suggestDrink(){
+//     axios
+//         .get('http://caffeinate.test/api/suggest-drink')
+//         .then(response => (app.suggestedNextDrink = response.data));
+// }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 @endpush
 
 @section('content')
-
+<div id='app'>
     <div class="jumbotron">
         <h1 class="display-4">Caffeinate! Dashboard</h1>
         <p class="lead">Track caffeine consumption... For safety!</p>
@@ -17,36 +17,109 @@
     </div>
 
     <div class="row">
+
         <div class="col-lg-6 col-sm-12">
-            
+            <h5>Suggestion Section</h5>
+            <div class="card"  v-if="suggestedNextDrink">
+                <div class="card-body">
+                    <h5 class="card-title">It looks like you have enough caffeine left for this tasty beverage:</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">
+                        @{{ suggestedNextDrink.drink_name }}
+                        <small>
+                            Servings: @{{ suggestedNextDrink.servings }}, Caffeine/serving: @{{ suggestedNextDrink.caffeine_amount }}
+                        </small>
+                    </h6>
+                    <p class="card-text">@{{ suggestedNextDrink.drink_description }}</p>
+                    <button  class="btn btn-success drink-now" v-on:click="drinkADrink(suggestedNextDrink.id)">Drink Now!</button>
+                </div>
+            </div>
+            <div class="card" v-else>
+                <div class="card-body">
+                    <h5 class="card-title">Have you considered a caffeine-free drink?</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">
+                        Maybe a nice herbal tea?
+                    </h6>
+                    <p class="card-text">Sorry, none of your favorite drinks have low enough amounts of caffeine to drink without putting you over your limit.</p>
+                    <p class="card-text small">You could always un-drink something...</p>
+                </div>
+            </div>
         </div>
         <div class="col-lg-6 col-sm-12">
+            <h5>Choose your own drink</h5>
+            <div class="row">
+                <div class="col-md-12">
+                    Remaining Caffeine: @{{ caffeineLeft }}
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    Not in the mood for the suggested drink?
+                    <div class="form-inline">
+                        <label class="mb-2 mr-sm-2">Select your drink here:</label>
+                        <select class="form-control mb-2 mr-sm-2" v-model="toDrink">
+                            <option v-for="drink in drinks" v-bind:value="drink">@{{ drink.drink_name }} - @{{ drink.caffeine_amount * drink.servings }}mg</option>
+                        </select>
+                    </div>
 
+
+                    <button class="btn btn-outline-success drink-choice" v-on:click="checkAndDrink(toDrink)">Drink Selected</button>
+                </div>
+            </div>
+            {{--Remaining Caffeine: @{{ caffeineLeft }}--}}
+            {{--Not in the mood for the suggested drink? Select your drink here: <select></select>--}}
         </div>
     </div>
     <div class="row">
-        <div class="col-lg-6 col-sm-12">
+        <div class="col-lg-12 col-sm-12">
+            <h5>Consumed Drinks</h5>
             <table class="table table-striped">
                 <thead class="thead-dark">
-                    <th>Drink Name</th>
-                    <th>Total Caffeine</th>
-                    <th>Delete Drink</th>
+                <th>Drink Name</th>
+                <th>Total Caffeine</th>
+                <th>Un-Drink</th>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>coffee</td>
-                        <td>75mg</td>
-                        <td><button class="btn btn-danger">Delete</button></td>
-                    </tr>
+                <tr v-for="drink in consumedDrinks">
+                    <td>@{{ drink.drink.drink_name }}</td>
+                    <td>@{{ drink.drink.caffeine_amount * drink.drink.servings }}mg</td>
+                    <td><button class="btn btn-danger" v-on:click="deleteDrink(drink.id)">Un-Drink</button></td>
+                </tr>
                 </tbody>
             </table>
         </div>
 
-        <div class="col-lg-6 col-sm-12">
-            Graphs
-        </div>
+        {{--<div class="col-lg-6 col-sm-12">--}}
+            {{--Graphs--}}
+
+
+        {{--</div>--}}
+
 
     </div>
+
+    <div id="myModal" class="modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Too Much Caffeine! :(</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Sorry, that drink contains enough caffeine to put you over your daily limit. Please check the drink suggestion section
+                    for an option that may work</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+
 
 
 
@@ -57,5 +130,7 @@
 @push('scripts')
     <!-- Load our React component. -->
     {{--<script type="text/javascript" src="./js/LikeButton.js"></script>--}}
+    <script type="text/javascript" src="./js/caffeinate.js"></script>
+
 
 @endpush
